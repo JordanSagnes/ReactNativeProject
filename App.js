d37dd809219408e390007be6663fab5bca938bba
Jordan <jordan.sagnes@orange.fr>
2019-11-23 11:34:39 +0100

    fix

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import RecipeDetails from "./src/components/RecipeDetails";
+import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RecipeDetails/>
+      <Navigation/>
     </View>
   );
 }
